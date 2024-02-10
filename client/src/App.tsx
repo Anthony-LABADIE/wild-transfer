897571f478ce45b602839563ed5927d02467b968
ANTHONY LABADIE <91564849+Anthony-LABADIE@users.noreply.github.com>
2024-02-11 00:55:01 +0100

    test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import Transition from './Transition'
 import UserContextProvider from './context/UserContext.tsx'
 
+//
 function App() {
   return (
     <>
